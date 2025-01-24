a573a9970fb893ab976f7aafeff71b3da5434496
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-01-24 18:01:18 +0100

    remove unecessary aws dynamo db table creation in main.tf

--- main.tf
+++ main.tf
@@ -28,20 +28,5 @@ provider "aws" {
   }
 }
 
-# Create DynamoDB table for state locking
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "game-jam-terraform-state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform State Lock Table"
-  }
-}
 
 # You can add your AWS resources here 
