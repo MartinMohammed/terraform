12957a343e97dcaedee738175165a007bb4826b7
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-01-24 19:32:02 +0100

    add ecs_role_policy, ecs_security_group and data_source.tf

---
+++ data_source.tf
@@ -0,0 +1,26 @@
+# Retrieve the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get all subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Use the first available subnet
+data "aws_subnet" "selected_subnet" {
+  id = tolist(data.aws_subnets.default.ids)[0]
+}
+
+# Retrieve the default security group within the default VPC
+data "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+}
