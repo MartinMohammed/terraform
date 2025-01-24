b9d3ee6836ff203164acdca905d14a7f2e802526
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-01-24 19:35:12 +0100

    remove aws_ecr_lifecycle_policy and update path to image for fast api service

--- ecr.tf
+++ ecr.tf
@@ -6,23 +6,3 @@ resource "aws_ecr_repository" "ElasticContainerRegistry" {
     scan_on_push = true
   }
 }
-
-# ECR Lifecycle policy to keep only last 5 images
-resource "aws_ecr_lifecycle_policy" "backend" {
-  repository = aws_ecr_repository.backend.name
-
-  policy = jsonencode({
-    rules = [{
-      rulePriority = 1
-      description  = "Keep last 5 images"
-      selection = {
-        tagStatus   = "any"
-        countType   = "imageCountMoreThan"
-        countNumber = 5
-      }
-      action = {
-        type = "expire"
-      }
-    }]
-  })
-}
