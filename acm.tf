19ce8d87997be29a12482796e2aad74d56507571
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-01-25 20:13:22 +0100

    add acm certificate request to sign a.therealfriends.com, create dns validation records in route53 hosted zone, auto. certificate validiation, alb https listeners and ingress configuration on ecs sg level.

---
+++ acm.tf
@@ -0,0 +1,43 @@
+# ACM Certificate for the subdomain
+resource "aws_acm_certificate" "subdomain_cert" {
+  domain_name       = "a.therealfriends.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Environment = "prod"
+    Project     = var.base_name
+  }
+}
+
+# Get the hosted zone for therealfriends.com
+data "aws_route53_zone" "main" {
+  name = "therealfriends.com"
+}
+
+# Create DNS validation records
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.subdomain_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.main.zone_id
+}
+
+# Certificate validation
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.subdomain_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
--- ecs.tf
+++ ecs.tf
@@ -185,6 +185,26 @@ resource "aws_lb_listener" "front_end" {
   protocol          = "HTTP"
 
   default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# HTTPS listener
+resource "aws_lb_listener" "front_end_https" {
+  for_each = local.environments
+
+  load_balancer_arn = aws_lb.ecs_alb[each.key].arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.subdomain_cert.arn
+
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ecs_tg[each.key].arn
   }
